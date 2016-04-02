a51e10ae0b3085091cc9f026f14bb875704fcccf
FlorianMnn <florian.minin@gmail.com>
2016-04-02 17:49:30 +0200

    Modification page d'accueil + Ajout logique MAJ titre onglet + Préparation logique récupération des données + Creation du NewsRepository

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,11 +13,16 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        //Récupération des news "En_Avant"
+        $entityManager = $this->getDoctrine()->getManager();
 
+        //Récupération des news "En_Avant"
+        $news = $entityManager->getRepository('AppBundle:News')->findAllNewsEnAvant();
+
+        dump($news);
         //Récupération des évènements à venir
+        //$news = $entityManager->getRepository('AppBundle:News')->findAll();
 
         //Selection du template de vue et passage des paramètres
-        return $this->render('default/index.html.twig', array('title' => "Acceuil"));
+        return $this->render('default/index.html.twig', array('title' => "Accueil"));
     }
 }
